42f4bf7828798a9e7d2cd98bf7f0ec370cb9589c
kazuya <oozoratiga@gmail.com>
2024-06-02 15:19:07 +0900

    コントローラー用のスクリプト作成

--- Assets/kazuya/Scriptfail/Controller.cs
+++ Assets/kazuya/Scriptfail/Controller.cs
@@ -5,34 +5,35 @@ using UnityEngine.InputSystem.Controls;
 
 public class Controller : MonoBehaviour
 {
-//    private  Rigidoby2D rigidoby2D;
-//    [SerializeField] public float dfSpeed;
-//    [SerializeField] public float xSpeed;//プレイヤーの速度
-//    public ButtonControl left { get; private set; }
-//    private GameObject characterChg;
-//    Spawn spawn;
+    private new Rigidbody2D rigidbody2D;
+    [SerializeField] public float dfSpeed;
+    [SerializeField] public float xSpeed;//プレイヤーの速度
+    public ButtonControl left { get; private set; }
+    private GameObject characterChg;
+    Spawn spawn;
+    PlayerController playerController;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        playerController = GetComponent<PlayerController>();
+    }
 
-//    void Start()
-//    {
-//        rigidoby2D = GetComponent<rigidoby2D>();
-//    }
 
- 
-//    void Update()
-//    {
-        
-//    }
+    void Update()
+    {
 
-//    private void FixedUpdate()
-//    {
-//        if (spawn.Alive)
-//        {
-//            xSpeed = dfSpeed;
-//            if (Input.GetAxis("CHorizontal"))
-//            {
-//                prri = true;
-//                rigidoby2D.velocity = new Vector2(xSpeed, rigidoby2D.velocity.y);
-//            }
-//        }
-//    }
+    }
+
+    private void FixedUpdate()
+    {
+        if (spawn.Alive)
+        {
+            xSpeed = dfSpeed;
+            if (Input.GetButton("CHorizontal"))
+            {
+                playerController.prri = true;
+                rigidbody2D.velocity = new Vector2(xSpeed, rigidbody2D.velocity.y);
+            }
+        }
+    }
 }
