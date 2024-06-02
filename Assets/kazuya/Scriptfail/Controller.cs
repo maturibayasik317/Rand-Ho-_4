3728e18bd3d08a4b0fce576883c6113b12b20ca9
kazuya <oozoratiga@gmail.com>
2024-06-02 15:03:14 +0900

    新しいスクリプトの追加

---
+++ Assets/kazuya/Scriptfail/Controller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class Controller : MonoBehaviour
+{
+//    private  Rigidoby2D rigidoby2D;
+//    [SerializeField] public float dfSpeed;
+//    [SerializeField] public float xSpeed;//プレイヤーの速度
+//    public ButtonControl left { get; private set; }
+//    private GameObject characterChg;
+//    Spawn spawn;
+
+//    void Start()
+//    {
+//        rigidoby2D = GetComponent<rigidoby2D>();
+//    }
+
+ 
+//    void Update()
+//    {
+        
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        if (spawn.Alive)
+//        {
+//            xSpeed = dfSpeed;
+//            if (Input.GetAxis("CHorizontal"))
+//            {
+//                prri = true;
+//                rigidoby2D.velocity = new Vector2(xSpeed, rigidoby2D.velocity.y);
+//            }
+//        }
+//    }
+}
